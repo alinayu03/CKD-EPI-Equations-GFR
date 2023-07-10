99db2449d007ff6518fd5ca05fd649ca2465a75a
John Harvardalinayu03 <alinayu@college.harvard.edu>
2023-07-10 16:54:51 -0400

    correct equations

--- main.py
+++ main.py
@@ -1,27 +1,96 @@
 import streamlit as st
-import numpy as np
 
-st.title("CKD-EPI GFR Calculator")
+def calculate_2021gfr(scr, age, sex):
+    # 2021 CKD-EPI creatinine equation for GFR
+    if sex == 'Male':
+        if scr <= 0.9:
+            A = 0.9
+            B = -0.302
+        else:
+            A = 0.9
+            B = -1.2
+        gfr = 142 * (scr/A)**B * 0.9938**age
+    elif sex == 'Female':
+        if scr <= 0.7:
+            A = 0.7
+            B = -0.241
+        else:
+            A = 0.7
+            B = -0.241
+        gfr = 142 * (scr/A)**B * 0.9938**age * 1.012
+    return gfr
 
+def calculate_2021gfrC(scr, scys, age, sex):
+    # 2021 CKD-EPI creatinine-crystatin C equation for GFR
+    if sex == 'Male':
+        if scr <= 0.9:
+            if scys <= 0.8:
+                A = 0.9
+                B = -0.144
+                C = 0.8
+                D = -0.323
+            elif scys > 0.8:
+                A = 0.9
+                B = -0.144
+                C = 0.8
+                D = -0.778
+        elif scr > 0.9: 
+            if scys <= 0.8:
+                A = 0.9
+                B = -0.544
+                C = 0.8
+                D = -0.323
+            elif scys > 0.8:
+                A = 0.9
+                B = -0.544
+                C = 0.8
+                D = -0.778
+        gfr = 135 * (scr/A)**B * (scys/C)**D * 0.9961**age
+    elif sex == 'Female':
+        if scr <= 0.7:
+            if scys <= 0.8:
+                A = 0.7
+                B = -0.219
+                C = 0.8
+                D = -0.323
+            elif scys > 0.8:
+                A = 0.7
+                B = -0.219
+                C = 0.8
+                D = -0.778
+        elif scr > 0.7:
+            if scys <= 0.8:
+                A = 0.7
+                B = -0.544
+                C = 0.8
+                D = -0.323
+            elif scys > 0.8:
+                A = 0.7
+                B = -0.544
+                C = 0.8
+                D = -0.778
+        gfr = 135 * (scr/A)**B * (scys/C)**D * 0.9961**age * 0.963
+    return gfr
 
-def calculate_gfr(age, gender, race, scr):
-    """
-    Calculate GFR using CKD-EPI equation
-    """
-    scr = scr / 0.9 if scr <= 0.9 else (scr / 0.9)**-1.209
-    age_factor = 0.993**age
-    gender_factor = 0.739 if gender == "female" else 1
-    race_factor = 1.159 if race == "black" else 1
-    return 141 * min(scr, 1)**-0.329 * max(scr, 1)**-1.209 * age_factor * gender_factor * race_factor
+# Streamlit app
+st.title('2021 CKD-EPI Creatinine Equations for Glomerular Filtration Rate')
+st.sidebar.title("Equation")
+equation = st.sidebar.radio("Select an Equation",
+                            ("2021 CKD-EPI Creatinine",
+                            "2021 CKD-EPI Creatinine-Crystatin C"))
 
+scr = st.number_input(
+    'Enter your Serum creatinine (mg/dL)', min_value=0.0, max_value=20.0, step=0.1)
+age = st.number_input('Enter your Age (years)', min_value=1, max_value=120, step=1)
+sex = st.selectbox('Select your Sex', options=['Male', 'Female'])
 
-age = st.number_input('Insert your age', min_value=1, max_value=120, value=50)
-gender = st.radio("Select your gender", ("male", "female"))
-race = st.radio("Select your race", ("black", "other"))
-scr = st.number_input('Insert your Serum Creatinine (mg/dL)',
-                      min_value=0.1, max_value=10.0, value=1.0)
+if equation == "2021 CKD-EPI Creatinine-Crystatin C":
+    scys = st.number_input(
+        'Enter your Serum cystatin C (mg/L)', min_value=0.0, max_value=20.0, step=0.1)
 
-if st.button('Calculate GFR'):
-    gfr = calculate_gfr(age, gender, race, scr)
-    st.write(
-        f'Your estimated Glomerular Filtration Rate (GFR) is {gfr:.2f} mL/min/1.73 m^2')
+if st.button('Calculate Glomerular Filtration Rate'):
+    if equation == "2021 CKD-EPI Creatinine":
+        gfr = calculate_2021gfr(scr, age, sex)
+    elif equation == "2021 CKD-EPI Creatinine-Crystatin C":
+        gfr = calculate_2021gfrC(scr, scys, age, sex)
+    st.write(f'Your GFR is {gfr:.2f} mL/min/1.73m².')
